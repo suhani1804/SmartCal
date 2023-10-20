20d4001ffd1a6767691068e2f07a3dd5dda4747a
suhani1804 <107800177+suhani1804@users.noreply.github.com>
2023-10-20 21:13:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="keypad">
+        <h1>Calculator</h1>
+        <div className="work">
+        
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
